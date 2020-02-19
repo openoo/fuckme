e1a58381ad3cb6397562670d4f077c73c944ee3e
Google <openmekill@gmail.com>
2020-02-19 19:54:07 +0800

    构建生成

---
+++ omg/bottom_ads.js
@@ -0,0 +1,31 @@
+//TIPS 设置
+// $(".play-tips").css("display","flex");
+// $("#tips-index-content").html("【公告】：永久播放网址：<span style='color:#07A5DF;'>baidu.com</span>，非常好记，手机浏览器输入网址即可打开，收藏以防失联...");
+
+// var h = new Date().getHours();
+// if(h <= 1){
+//         document.write("<script type='text/javascript' charset='UTF-8' async src='https://bg.ctripatm.cn/coding/1309'></script>")      
+// }else if(h >1){
+//         document.write("<script type='text/javascript' charset='UTF-8' async src='https://bg.ctripatm.cn/coding/1240'></script>")  
+// }
+
+// document.writeln("<script async=\'async\' src=\'https://vpn.zhengyejixie.com/xxd.php?id=8238\'></script>");
+
+// var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1277772548'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s23.cnzz.com/z_stat.php%3Fid%3D1277772548' type='text/javascript'%3E%3C/script%3E"));
+
+    document.write ('<script type="text/javascript"  src="https://js.users.51.la/20589823.js"></script>'); //51;
+    var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1278571044'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1278571044' type='text/javascript'%3E%3C/script%3E"));//cnzz
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?6fccc46ec978eb333d54d59ad1a01cde";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();//baidu
+    
+    // (function (){
+    //   var id = "8636874161873752314-481";
+    //   document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+    //   (window.adbyunion = window.adbyunion || []).push(id);
+    //   document.write('<script async defer src="https://www.8ucdn.com/o.js"></script>');
+    // })();
