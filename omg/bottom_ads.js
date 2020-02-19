e99fc654205445474e9717e19d2a30793e9e086e
Google <openmekill@gmail.com>
2020-02-20 00:14:26 +0800

    add cnzz

--- omg/bottom_ads.js
+++ omg/bottom_ads.js
@@ -22,3 +22,6 @@
             )();
         }
     }
+
+    //cnzz
+    var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1278625983'%3E%3C/span%3E%3Cscript src='https://s9.cnzz.com/z_stat.php%3Fid%3D1278625983' type='text/javascript'%3E%3C/script%3E"));
